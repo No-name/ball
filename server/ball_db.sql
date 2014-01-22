bcf0d76a20e31f5fab7370f0abb11beb8a59bca4
wangyong <yongwc@yeah.net>
2014-01-22 23:36:12 +0800

    create a new table to keep relationship

--- server/ball_db.sql
+++ server/ball_db.sql
@@ -35,7 +35,6 @@ SET default_with_oids = false;
 CREATE TABLE memberinfo (
     id integer NOT NULL,
     name character varying(80) NOT NULL,
-    relationship character varying(80)[],
     passwd character varying(50) NOT NULL
 );
 
@@ -64,6 +63,18 @@ ALTER SEQUENCE memberinfo_id_seq OWNED BY memberinfo.id;
 
 
 --
+-- Name: relationship; Type: TABLE; Schema: public; Owner: ball; Tablespace: 
+--
+
+CREATE TABLE relationship (
+    master character varying(80) NOT NULL,
+    member character varying(80) NOT NULL
+);
+
+
+ALTER TABLE public.relationship OWNER TO ball;
+
+--
 -- Name: id; Type: DEFAULT; Schema: public; Owner: ball
 --
 
@@ -74,7 +85,11 @@ ALTER TABLE ONLY memberinfo ALTER COLUMN id SET DEFAULT nextval('memberinfo_id_s
 -- Data for Name: memberinfo; Type: TABLE DATA; Schema: public; Owner: ball
 --
 
-COPY memberinfo (id, name, relationship, passwd) FROM stdin;
+COPY memberinfo (id, name, passwd) FROM stdin;
+1	Bob	123456
+4	Alan	guessword
+3	Alice	xyzllk
+2	Lisa	22345
 \.
 
 
@@ -82,7 +97,15 @@ COPY memberinfo (id, name, relationship, passwd) FROM stdin;
 -- Name: memberinfo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: ball
 --
 
-SELECT pg_catalog.setval('memberinfo_id_seq', 1, false);
+SELECT pg_catalog.setval('memberinfo_id_seq', 4, true);
+
+
+--
+-- Data for Name: relationship; Type: TABLE DATA; Schema: public; Owner: ball
+--
+
+COPY relationship (master, member) FROM stdin;
+\.
 
 
 --
@@ -91,6 +114,30 @@ SELECT pg_catalog.setval('memberinfo_id_seq', 1, false);
 
 ALTER TABLE ONLY memberinfo
     ADD CONSTRAINT memberinfo_pkey PRIMARY KEY (name);
+
+
+--
+-- Name: relationship_pkey; Type: CONSTRAINT; Schema: public; Owner: ball; Tablespace: 
+--
+
+ALTER TABLE ONLY relationship
+    ADD CONSTRAINT relationship_pkey PRIMARY KEY (master, member);
+
+
+--
+-- Name: relationship_master_fkey; Type: FK CONSTRAINT; Schema: public; Owner: ball
+--
+
+ALTER TABLE ONLY relationship
+    ADD CONSTRAINT relationship_master_fkey FOREIGN KEY (master) REFERENCES memberinfo(name);
+
+
+--
+-- Name: relationship_member_fkey; Type: FK CONSTRAINT; Schema: public; Owner: ball
+--
+
+ALTER TABLE ONLY relationship
+    ADD CONSTRAINT relationship_member_fkey FOREIGN KEY (member) REFERENCES memberinfo(name);
 
 
 --
